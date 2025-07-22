99c9fdbfecd1cd0f3128c2c8a602c6fe68a5f7af
Ritabrata Ray <43109771+ritabrata-ray@users.noreply.github.com>
2025-07-22 02:07:36 -0400

    Add files via upload

---
+++ losses/D_sing_space/NT-Logistic.py
@@ -0,0 +1,6 @@
+"""
+    This would be NTLogistic loss with a kernelized parameterization.
+    Need to be implemented in the same fashion as KLIEP.
+    But the current setting of batch data on x's and a's don't access cross samples. 
+    Need to check with ChePing on this.
+"""
